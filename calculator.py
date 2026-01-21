9152e5c4a38d031f6624d6350854a516c278e56e
That Arctic Penguin <anonymous837666@gmail.com>
2026-01-21 20:58:18 +0530

    Add basic calculator functionality in calculator.py

---
+++ calculator.py
@@ -0,0 +1,19 @@
+# Accept 2 numbers, num1 and num2
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Addition
+sum = num1 + num2
+print(f"Addition: {sum}")
+
+# Subtraction
+diff = num1 - num2
+print(f"Subtraction: {diff}")
+
+# Multiplication
+mult = num1*num2
+print(f"Multiplication: {mult}")
+
+# Division
+divi = num1/num2
+print(f"Division: {divi}")
